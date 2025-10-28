ba6970c01e731096d1c96b18915a21bee1be5fcc
alanCruzFC <acruzdelarosa.e@fincomun.com.mx>
2025-10-27 18:55:25 -0600

    registro de usuarios

--- backendbancoimagenes/src/main/java/com/fc/backendbancoimagenes/controller/AuthController.java
+++ backendbancoimagenes/src/main/java/com/fc/backendbancoimagenes/controller/AuthController.java
@@ -1,12 +1,18 @@
 package com.fc.backendbancoimagenes.controller;
 
 import com.fc.backendbancoimagenes.dto.AuthRequest;
+import com.fc.backendbancoimagenes.dto.RegisterRequest;
+import com.fc.backendbancoimagenes.model.Usuario;
+import com.fc.backendbancoimagenes.repository.UserRepository;
 import com.fc.backendbancoimagenes.security.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,5 +34,26 @@ public class AuthController {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         return jwtService.generateToken(userDetails);
     }
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("El usuario ya existe");
+        }
+
+        Usuario user = new Usuario();
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        userRepository.save(user);
+
+        return ResponseEntity.ok("Usuario creado exitosamente");
+    }
+
 }
 
