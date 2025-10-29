116c8c44bb8cd60e2671da57c9608d0ddaec887e
alanCruzFC <acruzdelarosa.e@fincomun.com.mx>
2025-10-29 13:59:39 -0600

    modified:   backendbancoimagenes/src/main/java/com/fc/backendbancoimagenes/BackendbancoimagenesApplication.java

--- backendbancoimagenes/src/main/java/com/fc/backendbancoimagenes/BackendbancoimagenesApplication.java
+++ backendbancoimagenes/src/main/java/com/fc/backendbancoimagenes/BackendbancoimagenesApplication.java
@@ -1,13 +1,7 @@
 package com.fc.backendbancoimagenes;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.fc.backendbancoimagenes.model.Usuario;
-import com.fc.backendbancoimagenes.repository.UserRepository;
 
 @SpringBootApplication
 public class BackendbancoimagenesApplication {
